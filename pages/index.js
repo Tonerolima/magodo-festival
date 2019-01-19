a1fcd1dc6ee47b7080ba11eb4fa831911bd6e4e1
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-19 14:40:32 +0100

    remove unused hooks code from index.js

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../static/styles/hamburger.css";
 import Page from "../layout/main";
 import Nav from "../components/common/navbar";
-import { useState } from "react";
 import Showcase from "../components/screens/home/showcase";
 
 export default () => {
-  const [hamburgerState, setHamburgerState] = useState("");
-
-  const toggleHamburgerState = () => {
-    if (hamburgerState === "") {
-      return setHamburgerState("is-active");
-    } else {
-      return setHamburgerState("");
-    }
-  };
-
   return (
     <Page>
       <Nav />
