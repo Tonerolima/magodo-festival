feb20e371c59c54ea839435be2cd355c4735cb8c
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-19 16:22:31 +0100

    design and implement about section

---
+++ components/screens/home/about.js
@@ -0,0 +1,77 @@
+import "../../../static/styles/fonts.css";
+import Link from "next/link";
+
+const AboutSection = () => {
+  return (
+    <section className="my-5 text-white">
+      <div className="container py-5">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-lg-6 d-flex flex-column text-right align-items-end">
+            <h2 className="">About the Carnival</h2>
+            <p className="underline mb-5" />
+            <p className="text">
+              The contemporary and traditional street carnival show will involve
+              the display of beautiful costumes by the contingents. This costume
+              includes the Western costumes used in carnivals like Notting Hill
+              carnival, Rio carnival, Trinidad carnival and our own Calabar
+              carnival. The costumes are created with accessories like
+              artificial feathers, glittering accessories, exotic sequence
+              materials of different colours and lots more. All these are
+              combined to bring out the aesthetics created by the designer to
+              make the carnival colorful. The designs are of different shapes
+              and styles like flowing gowns, miniskirts, flamboyant head hats
+              etc.to suite the purpose of the display. The traditional costumes
+              will involve the display of our own indigenous wears here in
+              Nigeria, thereby paying attention to the six Geo-political zones.
+              Difference kinds of dances are displayed as the contingent’s
+              dances to different recorded or live music which could be
+              contemporary or traditional songs. The displays will also involve
+              calisthenics of different formation from street to street within
+              Magodo to promote cultural unity.
+            </p>
+            <Link href="/about">
+              <button className="btn mb-5">
+                Read More <i className="fas fa-arrow-right ml-2" />
+              </button>
+            </Link>
+          </div>
+          <div className="col-lg-6" />
+        </div>
+        <div className="row" />
+      </div>
+      <style jsx>
+        {`
+          section {
+            font-family: "Sarabun", cursive;
+          }
+          h2 {
+            font-weight: 700;
+          }
+          .underline {
+            width: 100px;
+            border-bottom: 3px double #ffe400;
+          }
+          img {
+            box-shadow: inset 0px 0px 20px #272727;
+          }
+          .col-lg-6 + .col-lg-6 {
+            background: url("https://res.cloudinary.com/tonerolima/image/upload/v1547908054/magodo/smiling-young-lady.jpg");
+            background-position: center;
+            box-shadow: inset 0px 0px 20px 20px #272727;
+            height: 700px;
+            width: 700px;
+          }
+          .text {
+            line-height: 1.5rem;
+          }
+          .btn {
+            background-color: #ffe400;
+            color: #747474;
+          }
+        `}
+      </style>
+    </section>
+  );
+};
+
+export default AboutSection;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,22 @@ import "../static/styles/hamburger.css";
 import Page from "../layout/main";
 import Nav from "../components/common/navbar";
 import Showcase from "../components/screens/home/showcase";
+import AboutSection from "../components/screens/home/about";
 
 export default () => {
   return (
     <Page>
       <Nav />
       <Showcase />
+      <AboutSection />
+
+      <style>
+        {`
+            #page {
+              background-color: #272727;
+            }
+          `}
+      </style>
     </Page>
   );
 };
