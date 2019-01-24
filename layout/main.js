298f6fe9324d70b24acd568923441d8071c40c6b
Anthony Oyathelemhi <tonero91@gmail.com>
2019-01-24 01:49:30 +0100

    add meta tag for google search verification

--- layout/main.js
+++ layout/main.js
@@ -9,6 +9,10 @@ export default ({ children }) => (
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <meta
+        name="google-site-verification"
+        content="lrr1JNH6-v1vB7LQLA-I5QUgD46GycbNLy4GN6YZMxc"
+      />
       <link
         rel="stylesheet"
         href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
